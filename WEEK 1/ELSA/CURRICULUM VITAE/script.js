581eea01c60068c4c1df3a519e48c691b20c1e3b
fardillaelsa <elsaafardilla.gmailcom>
2025-07-16 15:07:14 +0700

    p 2

--- WEEK 1/ELSA/CURRICULUM VITAE/script.js
+++ WEEK 1/ELSA/CURRICULUM VITAE/script.js
@@ -1,6 +1,6 @@
 document.getElementById("whatsappBtn").addEventListener("click", function() {
     let phoneNumber = '6285812404727';
-    let message = 'Halo Kak! Bisa bayar COD atau transfer BCA, kak?';
+    let message = 'Halo Kak! apakah benar ini elsa?';
     let encodedMessage = encodeURIComponent(message); // menggunakan encodeURIComponent untuk memastikan pesan aman untuk URL
     let whatsappLink = `https://wa.me/${phoneNumber}?text=${encodedMessage}`; // membuat URL Whatsapp
     window.open(whatsappLink, '_blank'); // membuka jendela whatsapp baru 
